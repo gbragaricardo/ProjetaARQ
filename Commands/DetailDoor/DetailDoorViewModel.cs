983ab4cbd33f033a50f85434e642cb861b804f96
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2026-06-15 19:13:42 -0300

    update lang version create itemtemplate for viewoptions

--- Commands/DetailDoor/DetailDoorViewModel.cs
+++ Commands/DetailDoor/DetailDoorViewModel.cs
@@ -1,8 +1,24 @@
-﻿using ProjetaARQ.Core.UI;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace ProjetaARQ.Commands.DetailDoor
 {
-    public class DetailDoorViewModel : ObservableObject
+    public partial class DetailDoorViewModel : ObservableObject
     {
+        public ObservableCollection<ViewOptionItem> ViewOptionItems { get; } = new();
+
+        public DetailDoorViewModel()
+        {
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Ortogonal 3D" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Planta da Porta" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Corte A" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Corte B" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Elevação de Topo" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Elevação Inferior" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Elevação Esquerda" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Elevação Direita" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Elevação Frontal" });
+            ViewOptionItems.Add(new ViewOptionItem { ViewOptionName = "Elevação Posterior" });
+        }
     }
 }
--- Commands/DetailDoor/ViewOptionItem.cs
+++ Commands/DetailDoor/ViewOptionItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace ProjetaARQ.Commands.DetailDoor
 {
-    internal class ViewOptionItem
+    public partial class ViewOptionItem : ObservableObject
     {
+        [ObservableProperty]
+        private string _viewOptionName;
+
+        [ObservableProperty]
+        private bool _isSelected;
+
+        [ObservableProperty]
+        private string _selectedTemplate;
     }
 }
