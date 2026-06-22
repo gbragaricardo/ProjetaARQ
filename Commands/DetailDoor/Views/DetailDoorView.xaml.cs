ca399b852896fb93e3cec2cdd6e3b89f2f169af9
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2026-06-22 19:11:56 -0300

    change to fix current theme

--- Commands/DetailDoor/Views/DetailDoorView.xaml.cs
+++ Commands/DetailDoor/Views/DetailDoorView.xaml.cs
@@ -1,5 +1,7 @@
 using ProjetaARQ.Commands.DetailDoor.ViewModels;
 using ProjetaARQ.Core.UI;
+using System;
+using System.Linq;
 using System.Windows;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
@@ -8,6 +10,8 @@ namespace ProjetaARQ.Commands.DetailDoor.Views
 {
     public partial class DetailDoorView : FluentWindow
     {
+        private ApplicationTheme _currentTheme = ApplicationTheme.Light;
+
         // O construtor recebe a ViewModel via Injeção de Dependência do nosso contêiner Scoped
         public DetailDoorView(DetailDoorViewModel viewModel)
         {
@@ -20,23 +24,32 @@ namespace ProjetaARQ.Commands.DetailDoor.Views
 
         private void OnToggleThemeClicked(object sender, RoutedEventArgs e)
         {
-            // 1. Pega o tema atual
-            var currentTheme = ApplicationThemeManager.GetAppTheme();
-            var newTheme = currentTheme == ApplicationTheme.Light
-                ? ApplicationTheme.Dark
-                : ApplicationTheme.Light;
+            try
+            {
+                // 1. Alterna o estado local do tema
+                _currentTheme = _currentTheme == ApplicationTheme.Light
+                    ? ApplicationTheme.Dark
+                    : ApplicationTheme.Light;
 
-            // 2. Troca o tema (O Lepo agora vai conseguir achar a âncora no XAML e mudar tudo)
-            ApplicationThemeManager.Apply(newTheme);
-            ApplicationThemeManager.Apply(this);
+                // 2. Localiza o ThemesDictionary definido localmente na janela e aplica o tema
+                var themesDict = this.Resources.MergedDictionaries.OfType<Wpf.Ui.Markup.ThemesDictionary>().FirstOrDefault();
+                if (themesDict != null)
+                {
+                    themesDict.Theme = _currentTheme;
+                }
 
-            // 3. Atropela o reset do Lepo e devolve o Vermelho Projeta para a interface
-            UiResourceManager.ApplyProjetaAccents();
+                // 3. Atualiza o ícone da lua/sol correspondente
+                ThemeIcon.Symbol = _currentTheme == ApplicationTheme.Light
+                    ? SymbolRegular.WeatherMoon24
+                    : SymbolRegular.WeatherSunny24;
 
-            // 4. Atualiza o ícone da lua/sol
-            ThemeIcon.Symbol = newTheme == ApplicationTheme.Light
-                ? SymbolRegular.WeatherMoon24
-                : SymbolRegular.WeatherSunny24;
+                // 4. Reaplica as cores de acento do vermelho Projeta
+                UiResourceManager.ApplyProjetaAccents();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Erro ao alternar o tema localmente: {ex.Message}");
+            }
         }
     }
 }
