ed0af0f1efa1337ff9c01b968756cd1e5e9304fb
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2026-06-23 11:16:25 -0300

    improve theme change on codebehind

--- Commands/DetailDoor/Views/DetailDoorView.xaml.cs
+++ Commands/DetailDoor/Views/DetailDoorView.xaml.cs
@@ -10,51 +10,23 @@ namespace ProjetaARQ.Commands.DetailDoor.Views
 {
     public partial class DetailDoorView : FluentWindow
     {
-        private ApplicationTheme _currentTheme = ApplicationTheme.Light;
+        private readonly IThemeService _themeService;
 
-        // O construtor recebe a ViewModel via Injeção de Dependência do nosso contêiner Scoped
-        public DetailDoorView(DetailDoorViewModel viewModel)
+        public DetailDoorView(DetailDoorViewModel viewModel, IThemeService themeService)
         {
             InitializeComponent();
 
-            // Define o DataContext para o MVVM funcionar de forma limpa
+            _themeService = themeService;
             DataContext = viewModel;
+
             viewModel.RequestClose += () => this.DialogResult = true;
 
-            // Inicializa de acordo com o tema global do Revit/aplicação em memória
-            _currentTheme = UiResourceManager.GetCurrentTheme();
-            UpdateThemeIcon();
+            WindowThemeHelper.InitializeTheme(ThemeIcon, _themeService);
         }
 
         private void OnToggleThemeClicked(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                // 1. Alterna o estado local do tema
-                _currentTheme = _currentTheme == ApplicationTheme.Light
-                    ? ApplicationTheme.Dark
-                    : ApplicationTheme.Light;
-
-                // 2. Aplica o tema globalmente no nível da aplicação em memória
-                UiResourceManager.SetTheme(_currentTheme);
-
-                // 3. Atualiza o ícone da lua/sol correspondente
-                UpdateThemeIcon();
-
-                // 4. Reaplica as cores de acento do vermelho Projeta
-                UiResourceManager.ApplyProjetaAccents();
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Erro ao alternar o tema global: {ex.Message}");
-            }
-        }
-
-        private void UpdateThemeIcon()
-        {
-            ThemeIcon.Symbol = _currentTheme == ApplicationTheme.Light
-                ? SymbolRegular.WeatherMoon24
-                : SymbolRegular.WeatherSunny24;
+            WindowThemeHelper.ToggleTheme(ThemeIcon, _themeService);
         }
     }
 }
---
+++ Core/UI/WindowThemeHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+namespace ProjetaARQ.Core.UI
+{
+    public static class WindowThemeHelper
+    {
+        public static void InitializeTheme(SymbolIcon themeIcon, IThemeService themeService)
+        {
+            var theme = themeService.GetCurrentTheme();
+            UpdateIcon(themeIcon, theme);
+        }
+        public static ApplicationTheme ToggleTheme(SymbolIcon themeIcon, IThemeService themeService)
+        {
+            var current = themeService.GetCurrentTheme();
+            var next = current == ApplicationTheme.Light ? ApplicationTheme.Dark : ApplicationTheme.Light;
+
+            themeService.SetTheme(next);
+            UpdateIcon(themeIcon, next);
+            themeService.ApplyProjetaAccents();
+
+            return next;
+        }
+        private static void UpdateIcon(SymbolIcon themeIcon, ApplicationTheme theme)
+        {
+            if (themeIcon != null)
+            {
+                themeIcon.Symbol = theme == ApplicationTheme.Light
+                    ? SymbolRegular.WeatherMoon24
+                    : SymbolRegular.WeatherSunny24;
+            }
+        }
+    }
+}
